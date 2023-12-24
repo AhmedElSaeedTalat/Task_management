6d477bec80e1c4b531beed8c783b6bb4b5705c2f
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-12-24 18:47:04 +0000

    add view for home

--- manageTask/views.py
+++ manageTask/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+""" views """
 
-# Create your views here.
+def home(request):
+    """ home view """
+    return HttpResponse('this is home page')
+
